79e2d35fa461fbfb761733f3665c9f42c7581308
Mega-Sim <sik1093@naver.com>
2025-08-25 22:21:46 +0900

    Add files via upload

---
+++ linux/ebox/CMakeFiles/ebox.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ebox.dir/ebox.c.o"
+  "ebox.pdb"
+  "ebox"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ebox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
